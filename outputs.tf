7f3f581aee19b5b286fbaeb4d676a202e3b23814
washmyfaceinacid <superxman402@gmail.com>
2026-02-02 11:32:45 +0200

    Added terraform infra v0.2

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,22 @@
+# Notice we use 'module.bestrong_aks' instead of 'azurerm_resource_group'
+
 output "resource_group_name" {
   description = "The name of the created Resource Group"
-  value       = azurerm_resource_group.rg.name
+  value       = module.bestrong_aks.resource_group_name
 }
 
 output "acr_login_server" {
   description = "The login server URL for the Azure Container Registry"
-  value       = azurerm_container_registry.acr.login_server
-}
-
-output "acr_admin_username" {
-  description = "The admin username for ACR"
-  value       = azurerm_container_registry.acr.admin_username
-}
-
-output "acr_admin_password" {
-  description = "The admin password for ACR"
-  value       = azurerm_container_registry.acr.admin_password
-  sensitive   = true
+  value       = module.bestrong_aks.acr_login_server
 }
 
 output "aks_cluster_name" {
   description = "The name of the AKS Cluster"
-  value       = azurerm_kubernetes_cluster.aks.name
+  value       = module.bestrong_aks.aks_cluster_name
 }
 
 output "kube_config" {
   description = "Raw Kubernetes config to be used by kubectl"
-  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
+  value       = module.bestrong_aks.kube_config
   sensitive   = true
 }
